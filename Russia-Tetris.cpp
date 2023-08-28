d106c52a9b48f7a83debfdc29ad605fb014946b3
22222sss <1348291944@qq.com>
2023-08-28 17:51:12 +0800

    初始化界面

--- Russia-Tetris.cpp
+++ Russia-Tetris.cpp
@@ -1,20 +1,92 @@
-﻿// Russia-Tetris.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include<iostream>
+#include <termios.h>
+#include <fcntl.h>
+using namespace std;
+#define ROW 24 //游戏区行数
+#define COL 20 //游戏区列数
 
-#include <iostream>
+#define DOWN  's'//方向键：下
+#define LEFT  'a'//方向键：左
+#define RIGHT 'd'//方向键：右
+#define SPACE 32 //空格键
+#define ESC "\033" //Esc键
 
-int main()
+struct Face
 {
-    std::cout << "Hello World!\n";
+	int data[ROW][COL + 10];//用于标记指定位置是否有方块（1为有，0为无）
+	int color[ROW][COL + 10];//用于记录指定位置的方块颜色编码
+}face;
+
+struct Block
+{
+	int space[4][4];
+}block[7][4];//用于存储7种基本形状方块的各自的4种形态的信息，共28种
+
+int grade; //全局变量
+
+void InitInterface();//初始化界面
+void moveTo(int row, int col);//输出跳转函数
+void InitBlockInfo();//初始化方块信息
+void color(int num);//颜色设置
+//画出方块
+void DrawBlock(int shape, int form, int x, int y);
+//空格覆盖
+void DrawSpace(int shape, int form, int x, int y);
+//合法性判断
+int IsLegal(int shape, int form, int x, int y);
+//判断得分与结束
+int JudeFunc();
+//游戏主体逻辑函数
+void StartGame();
+//主函数
+int main();
+
+void moveTo(int row, int col)//移动到第row行第col列
+{
+	printf("\x1b[%d;%dH", row, col);
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+void InitInterface()//初始化界面
+{
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < COL + 10; j++)
+		{
+			if (j == 0 || j == COL - 1 || j == COL + 9)
+			{
+				face.data[i][j] = 1; //标记该位置有方块
+				moveTo(i + 1, 2 * j + 1);//方块占一个行位置，两个列位置
+				printf("■");
+			}
+			else if (i == ROW - 1)
+			{
+				face.data[i][j] = 1; //标记该位置有方块
+				moveTo(i + 1, 2 * j + 1);
+				printf("■");
+			}
+			else
+				face.data[i][j] = 0; //标记该位置无方块
+		}
+
+	}
+
+	for (int i = COL; i < COL + 10; i++)
+	{
+		face.data[11][i] = 1; //标记该位置有方块
+		moveTo(11 + 1, 2 * i + 1);
+		printf("■");
+	}
+
+	moveTo(2, 2 * COL + 1 + 1);
+	printf("\33[40m");
+	printf("Next:");
+
+	moveTo(14, 2 * COL + 2);
+	printf("\33[40m");
+	printf("Score:%d", grade);
+}
